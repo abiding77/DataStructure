3037317d6c71b85b51ef7e7fecbeef2aa1c64e32
Jung Ji Won <abiding77@naver.com>
2022-06-15 16:20:58 +0900

    최소힙 기반 우선순위 큐

--- W10_P1.cpp
+++ W10_P1.cpp
@@ -1,23 +1,10 @@
 #include <iostream>
-
 #include <vector>
-
 #include <string>
-
-
 
 using namespace std;
 
-// heap-order
-
-// 루트를 제외한 모든 트리의 우선순위는 부모 노드의 우선순위보다 낮거나 같음
-
-// min heap key(parent(v)) < key(v)
-
-// max heap key(parent(v)) >= key(v)
-
 class heap {
-
 private:
 	vector<int> arr;
 	void swap(int idx1, int idx2) {
@@ -28,7 +15,7 @@ private:
 	void upHeap(int idx) {
 		if (idx == 1) return;
 		int parent = idx / 2;
-		if (arr[parent] < arr[idx]) {
+		if (arr[parent] > arr[idx]) {
 			swap(parent, idx);
 			upHeap(parent);
 		}
@@ -37,16 +24,19 @@ private:
 		int left = 2 * idx;
 		int right = 2 * idx + 1;
 		int child;
-		if (left > size()) return;
+
+		if (left > size()) {
+			return;
+		}
 		else if (left == size()) {
 			child = left;
 		}
 		else {
-			if (arr[left] > arr[right]) child = left;
+			if (arr[left] <= arr[right]) child = left;
 			else child = right;
 		}
 
-		if (arr[child] > arr[idx]) {
+		if (arr[child] < arr[idx]) {
 			swap(child, idx);
 			downHeap(child);
 		}
@@ -55,19 +45,19 @@ public:
 	heap() {
 		arr.push_back(0);
 	}
-
 	int size() {
 		return arr.size() - 1;
-
 	}
-
 	bool empty() {
 		return (arr.size() == 1);
 	}
-
 	void insert(int e) {
 		arr.push_back(e);
 		upHeap(size());
+	}
+	int top() {
+		if (empty()) return -1;
+		return arr[1];
 	}
 	void pop() {
 		if (empty()) {
@@ -79,61 +69,44 @@ public:
 		arr.pop_back();
 		downHeap(1);
 	}
-
 	void print() {
-
 		if (empty()) {
 			cout << "-1" << endl;
 			return;
 		}
-
-		for (int i = 1; i < arr.size(); i++) {
+		for (int i = 1; i<arr.size(); i++) {
 			cout << arr[i] << " ";
 		}
 		cout << endl;
 	}
-
-	int top() {
-		if (empty()) {
-			return -1;
-		}
-		return arr[1];
-	}
 };
 
-
-
 int main() {
-
-	int input = 0;
-	cin >> input;
+	int testcase = 0;
+	cin >> testcase;
 	heap hp;
-
-	for (int i = 0; i < input; i++) {
-
+	for (int i = 0; i < testcase; i++) {
 		string s;
 		cin >> s;
-
-		if (s == "isEmpty") {
-			cout << hp.empty() << endl;
-		}
 		if (s == "insert") {
-			int num;
-			cin >> num;
-			hp.insert(num);
-		}
-
-		if (s == "print") {
-			hp.print();
+			int a;
+			cin >> a;
+			hp.insert(a);
 		}
 		if (s == "size") {
 			cout << hp.size() << endl;
 		}
+		if (s == "isEmpty") {
+			cout << hp.empty() << endl;
+		}
+		if (s == "pop") {
+			hp.pop();
+		}
 		if (s == "top") {
 			cout << hp.top() << endl;
 		}
-		if (s == "pop"){
-			hp.pop();
+		if (s == "print") {
+			hp.print();
 		}
 	}
 	return 0;
